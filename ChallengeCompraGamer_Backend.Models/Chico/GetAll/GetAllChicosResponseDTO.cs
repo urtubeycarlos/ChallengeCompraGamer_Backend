51d3228eb8cdf930b4fabaa8119a22258f856c25
challengecg1 <challenge_cg1@carlosurtubey.com>
2025-10-14 15:51:10 -0300

    DTO's para los GET de chicos

---
+++ ChallengeCompraGamer_Backend.Models/Chico/GetAll/GetAllChicosResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Chico.GetAll
+{
+    public class GetAllChicosResponseDTO
+    {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
