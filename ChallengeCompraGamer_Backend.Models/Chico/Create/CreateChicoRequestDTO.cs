c64e2c4e0a6a26f1952d79f2165fbd99c4a37e03
challengecg1 <challenge_cg1@carlosurtubey.com>
2025-10-14 15:51:26 -0300

    DTO's para la creación de chicos

---
+++ ChallengeCompraGamer_Backend.Models/Chico/Create/CreateChicoRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Chico.Create
+{
+    public class CreateChicoRequestDTO
+    {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+    }
+}
