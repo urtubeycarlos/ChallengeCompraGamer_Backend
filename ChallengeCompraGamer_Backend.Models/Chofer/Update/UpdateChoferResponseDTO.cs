1932413d654e792e6e0a3e6a17975c60795fb8e5
challengecg2 <challenge_cg2@carlosurtubey.com>
2025-10-14 17:37:42 -0300

    DTO's para la actualizació de chofer

---
+++ ChallengeCompraGamer_Backend.Models/Chofer/Update/UpdateChoferResponseDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Chofer.Update
+{
+    public class UpdateChoferResponseDTO
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string ClaseLicencia { get; set; }
+        public int Telefono { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
