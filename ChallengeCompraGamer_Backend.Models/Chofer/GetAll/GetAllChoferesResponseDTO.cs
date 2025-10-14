f3a5425ff4242082777ea149b730f8d8eacee42c
challengecg2 <challenge_cg2@carlosurtubey.com>
2025-10-14 17:36:45 -0300

    DTO's para la obtención de choferes o chofer by DNI

---
+++ ChallengeCompraGamer_Backend.Models/Chofer/GetAll/GetAllChoferesResponseDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Chofer.GetAll
+{
+    public class GetAllChoferesResponseDTO
+    {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string ClaseLicencia { get; set; }
+        public int Telefono { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
