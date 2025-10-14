b369ece3fbc3965b015e2521ce92300226a4fc29
challengecg2 <challenge_cg2@carlosurtubey.com>
2025-10-14 17:41:24 -0300

    Command, Handler y Validator para la eliminación de Chofer

---
+++ ChallengeCompraGamer_Backend.App/Commands/Chofer/DeleteChofer/DeleteChoferCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChallengeCompraGamer_Backend.App.Commands.Chofer.DeleteChofer
+{
+    public class DeleteChoferCommandValidator : AbstractValidator<DeleteChoferCommand>
+    {
+        public DeleteChoferCommandValidator()
+        {
+            RuleFor(x => x.DNI)
+                .NotEmpty().WithMessage("El DNI no puede estar vacío.")
+                .MaximumLength(16).WithMessage("El DNI no puede tener más de 16 caracteres.");
+        }
+    }
+}
