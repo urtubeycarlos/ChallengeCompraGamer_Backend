68e90f5163d20625bfccf3895ab1f64c5522ef60
challengecg1 <challenge_cg1@carlosurtubey.com>
2025-10-14 15:56:23 -0300

    Command, Handler y Validator para la actualización de un chico

---
+++ ChallengeCompraGamer_Backend.App/Commands/Chico/UpdateChico/UpdateChicoCommand.cs
@@ -0,0 +1,12 @@
+﻿using ChallengeCompraGamer_Backend.Models;
+using ChallengeCompraGamer_Backend.Models.Chico.Update;
+using MediatR;
+
+namespace ChallengeCompraGamer_Backend.App.Commands.Chico.UpdateChico
+{
+    public class UpdateChicoCommand : IRequest<Result<UpdateChicoResponseDTO>>
+    {
+        public string DNI { get; set; }
+        public UpdateChicoRequestDTO Body { get; set; }
+    }
+}
---
+++ ChallengeCompraGamer_Backend.App/Commands/Chico/UpdateChico/UpdateChicoCommandValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+
+namespace ChallengeCompraGamer_Backend.App.Commands.Chico.UpdateChico
+{
+    public class UpdateChicoCommandValidator : AbstractValidator<UpdateChicoCommand>
+    {
+        public UpdateChicoCommandValidator()
+        {
+            RuleFor(x => x.DNI)
+                .NotEmpty().WithMessage("El DNI no puede estar vacío.")
+                .MaximumLength(16).WithMessage("El DNI no puede tener más de 16 caracteres.");
+
+            RuleFor(x => x.Body)
+                .NotNull().WithMessage("El cuerpo de la solicitud no puede ser nulo.")
+                .ChildRules(body =>
+                {
+                    body.RuleFor(b => b.Nombre)
+                        .NotEmpty().WithMessage("El nombre no puede estar vacío.")
+                        .MaximumLength(128).WithMessage("El nombre no puede tener más de 128 caracteres.");
+                    body.RuleFor(b => b.Apellido)
+                        .NotEmpty().WithMessage("El apellido no puede estar vacío.")
+                        .MaximumLength(128).WithMessage("El apellido no puede tener más de 128 caracteres.");
+                });
+        }
+    }
+}
