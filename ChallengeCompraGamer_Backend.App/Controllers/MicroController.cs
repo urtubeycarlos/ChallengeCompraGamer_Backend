4cfe86220dddcab12c5f3af1a03de5172b8269c1
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-14 01:03:52 -0300

    GetMicros y GetMicro

--- ChallengeCompraGamer_Backend.App/Controllers/MicroController.cs
+++ ChallengeCompraGamer_Backend.App/Controllers/MicroController.cs
@@ -1,4 +1,6 @@
-﻿using MediatR;
+﻿using ChallengeCompraGamer_Backend.App.Commands.Micro.GetMicro;
+using ChallengeCompraGamer_Backend.App.Commands.Micro.GetMicros;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChallengeCompraGamer_Backend.Controllers
@@ -7,5 +9,36 @@ namespace ChallengeCompraGamer_Backend.Controllers
     [Route("[controller]")]
     public class MicroController : ControllerBase
     {
+        private readonly IMediator _mediator;
+        
+        public MicroController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var result = await _mediator.Send(new GetMicrosCommandRequest());
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+
+            return StatusCode((int) result.StatusCode, result.Message);
+        }
+
+        [HttpGet("{patente}")]
+        public async Task<IActionResult> Get(string patente)
+        {
+            var query = new GetMicroCommandRequest(patente);
+            var result = await _mediator.Send(query);
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+
+            return StatusCode((int) result.StatusCode, result.Message);
+        }
     }
 }
