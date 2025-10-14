214f38b8aff25cf787d9de6f56d416ecd6b4e5ea
challengecg2 <challenge_cg2@carlosurtubey.com>
2025-10-14 17:41:51 -0300

    Controller con CRUD básico para Chofer

--- ChallengeCompraGamer_Backend.App/Controllers/ChoferController.cs
+++ ChallengeCompraGamer_Backend.App/Controllers/ChoferController.cs
@@ -1,4 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ChallengeCompraGamer_Backend.App.Commands.Chofer.CreateChofer;
+using ChallengeCompraGamer_Backend.App.Commands.Chofer.DeleteChofer;
+using ChallengeCompraGamer_Backend.App.Commands.Chofer.GetChofer;
+using ChallengeCompraGamer_Backend.App.Commands.Chofer.GetChoferes;
+using ChallengeCompraGamer_Backend.App.Commands.Chofer.UpdateChofer;
+using ChallengeCompraGamer_Backend.Models.Chofer.Create;
+using ChallengeCompraGamer_Backend.Models.Chofer.GetByDNI;
+using ChallengeCompraGamer_Backend.Models.Chofer.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ChallengeCompraGamer_Backend.Controllers
 {
@@ -6,5 +15,89 @@ namespace ChallengeCompraGamer_Backend.Controllers
     [Route("[controller]")]
     public class ChoferController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public ChoferController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllChoferes()
+        {
+            var result = await _mediator.Send(new GetChoferesCommand());
+            
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return StatusCode((int)result.StatusCode, result.Message);
+            }
+        }
+
+        [HttpGet("{dni}")]
+        public async Task<IActionResult> GetChoferByDNI(string dni)
+        {
+            GetChoferByDniCommand query = new GetChoferByDniCommand(dni);
+            var result = await _mediator.Send(query);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return StatusCode((int)result.StatusCode, result.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] CreateChoferRequestDTO request)
+        {
+            CreateChoferCommand command = new CreateChoferCommand { Body = request };
+            var result = await _mediator.Send(command);
+            if (result.IsSuccess)
+            {
+                return Created($"/Chofer/{result.Data.DNI}", result.Data);
+            }
+            else
+            {
+                return StatusCode((int)result.StatusCode, result.Message);
+            }
+        }
+
+        [HttpPatch("{dni}")]
+        public async Task<IActionResult> Patch(string dni, UpdateChoferRequestDTO request)
+        {
+            UpdateChoferCommand command = new UpdateChoferCommand() { DNI = dni, Body = request };
+            var result = await _mediator.Send(command);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return StatusCode((int)result.StatusCode, result.Message);
+            }
+        }
+
+        [HttpDelete("{dni}")]
+        public async Task<IActionResult> Delete(string dni)
+        {
+            DeleteChoferCommand query = new DeleteChoferCommand(dni);
+            var result = await _mediator.Send(query);
+
+            if (result.IsSuccess)
+            {
+                return Ok(result.Data);
+            }
+            else
+            {
+                return StatusCode((int)result.StatusCode, result.Message);
+            }
+        }
     }
 }
