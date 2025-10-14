d843c250acc76b5090352a8204756139e575e7c3
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-14 01:04:40 -0300

    GetMicros y GetMicro en el servicio

--- ChallengeCompraGamer_Backend.Services/MicroService.cs
+++ ChallengeCompraGamer_Backend.Services/MicroService.cs
@@ -1,6 +1,46 @@
-﻿namespace ChallengeCompraGamer_Backend.Services
+﻿using ChallengeCompraGamer_Backend.DataAccess.Context;
+using ChallengeCompraGamer_Backend.DataAccess.Entities;
+using ChallengeCompraGamer_Backend.Models.Micro;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace ChallengeCompraGamer_Backend.Services
 {
     public class MicroService
     {
+        private readonly ChallengeCompraGamerContext _context;
+
+        public MicroService(ChallengeCompraGamerContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<MicroDTO>> Get()
+        {
+            IEnumerable<MicroDTO> micros = await _context.Micros.Select(m => new MicroDTO
+            {
+                Patente = m.Patente,
+                CreatedAt = m.CreatedAt,
+                UpdatedAt = m.UpdatedAt
+            }).ToListAsync();
+
+            return micros;
+        }
+
+        public async Task<MicroDTO> Get(string patente)
+        {
+            Micro micro = await _context.Micros.FindAsync(patente);
+            if (micro == null)
+            {
+                throw new KeyNotFoundException($"Micro with patente {patente} not found.");
+            }
+
+            return new MicroDTO
+            {
+                Patente = micro.Patente,
+                CreatedAt = micro.CreatedAt,
+                UpdatedAt = micro.UpdatedAt
+            };
+        }
     }
 }
