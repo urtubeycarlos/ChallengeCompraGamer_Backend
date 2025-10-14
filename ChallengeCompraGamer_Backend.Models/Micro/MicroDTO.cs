e45aa269ea6a6bd89e17b9b75d9d47c68fc05514
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-14 01:00:09 -0300

    MicroDTO

---
+++ ChallengeCompraGamer_Backend.Models/Micro/MicroDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Micro
+{
+    public class MicroDTO
+    {
+        public string Patente { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
