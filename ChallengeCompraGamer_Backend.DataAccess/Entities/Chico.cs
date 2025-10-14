31bbff901eb0c0fe19e386a401fe06d65815500f
challengecg1 <challenge_cg1@carlosurtubey.com>
2025-10-14 15:48:49 -0300

    entidad Chico y su relación con Micro

--- ChallengeCompraGamer_Backend.DataAccess/Entities/Chico.cs
+++ ChallengeCompraGamer_Backend.DataAccess/Entities/Chico.cs
@@ -2,5 +2,14 @@
 {
     public class Chico
     {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+
+        // Relación con Micro
+        public string? PatenteMicro { get; set; }
+        public Micro? Micro { get; set; }
     }
 }
