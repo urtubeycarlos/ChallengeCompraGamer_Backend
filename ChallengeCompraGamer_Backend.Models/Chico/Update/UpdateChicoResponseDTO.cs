27fde36a6a58f322f8f74177aed8badb77ef6a61
challengecg1 <challenge_cg1@carlosurtubey.com>
2025-10-14 15:53:23 -0300

    DTO's para la actualización de un chico

---
+++ ChallengeCompraGamer_Backend.Models/Chico/Update/UpdateChicoResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Chico.Update
+{
+    public class UpdateChicoResponseDTO
+    {
+        public string DNI { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
