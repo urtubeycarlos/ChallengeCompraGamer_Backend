6fd260102ad73293b4478016ed4886ea969dd290
challengecg2 <challenge_cg2@carlosurtubey.com>
2025-10-14 17:41:34 -0300

    Command, Handler y Validator para creación de Chofer

---
+++ ChallengeCompraGamer_Backend.App/Commands/Chofer/CreateChofer/CreateChoferCommand.cs
@@ -0,0 +1,11 @@
+﻿using ChallengeCompraGamer_Backend.Models;
+using ChallengeCompraGamer_Backend.Models.Chofer.Create;
+using MediatR;
+
+namespace ChallengeCompraGamer_Backend.App.Commands.Chofer.CreateChofer
+{
+    public class CreateChoferCommand : IRequest<Result<CreateChoferResponseDTO>>
+    {
+        public CreateChoferRequestDTO Body { get; set; }
+    }
+}
