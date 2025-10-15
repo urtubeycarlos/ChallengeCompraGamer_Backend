a1c6157b80c37a1caef2773f1e65bbf4a09f15d9
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-15 11:33:59 -0300

    DTO's para la obtención de micros

---
+++ ChallengeCompraGamer_Backend.Models/Micro/GetByPatente/GetMicroByPatenteResponseDTO.cs
@@ -0,0 +1,20 @@
+﻿using ChallengeCompraGamer_Backend.Models.Chico.GetAll;
+using ChallengeCompraGamer_Backend.Models.Chofer.GetAll;
+
+namespace ChallengeCompraGamer_Backend.Models.Micro.GetByPatente
+{
+    public class GetMicroByPatenteResponseDTO
+    {
+        public string Patente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int CantidadAsientos { get; set; }
+        public int CantidadAsientosOcupados { get; set; }
+        public bool EstaCompleto { get; set; }
+        public bool EstaAsignado { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public ICollection<GetAllChicosResponseDTO> Chicos { get; set; } = new List<GetAllChicosResponseDTO>();
+        public GetAllChoferesResponseDTO Chofer { get; set; } = null!;
+    }
+}
