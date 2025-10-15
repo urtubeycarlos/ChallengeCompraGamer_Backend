628ed4cbc1ec1a230e1c26d454fe336592b1cce2
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-15 11:34:29 -0300

    DTO's para la actualización de micro

---
+++ ChallengeCompraGamer_Backend.Models/Micro/Update/UpdateMicroResponseDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace ChallengeCompraGamer_Backend.Models.Micro.Update
+{
+    public class UpdateMicroResponseDTO
+    {
+        public string Patente { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int CantidadAsientos { get; set; }
+        public bool EstaCompleto { get; set; }
+        public bool EstaAsignado { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
