7783210fac93416f10770c0fb13444b5f32d9917
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-15 11:37:45 -0300

    Command, Handler y Validator para la creación de micro

---
+++ ChallengeCompraGamer_Backend.App/Commands/Micro/CreateMicro/CreateMicroCommand.cs
@@ -0,0 +1,11 @@
+﻿using ChallengeCompraGamer_Backend.Models;
+using ChallengeCompraGamer_Backend.Models.Micro.Create;
+using MediatR;
+
+namespace ChallengeCompraGamer_Backend.App.Commands.Micro.CreateMicro
+{
+    public class CreateMicroCommand : IRequest<Result<CreateMicroResponseDTO>>
+    {
+        public CreateMicroRequestDTO Body { get; set; }
+    }
+}
