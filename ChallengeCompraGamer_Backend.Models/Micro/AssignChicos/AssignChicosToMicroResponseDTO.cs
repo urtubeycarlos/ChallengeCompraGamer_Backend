8c90b1752cbd1b4eabb3e16b8bce8db8d6374e85
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2025-10-15 11:34:45 -0300

    DTO's para la asignacion de chicos a micro

---
+++ ChallengeCompraGamer_Backend.Models/Micro/AssignChicos/AssignChicosToMicroResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿using ChallengeCompraGamer_Backend.Models.Micro.GetByPatente;
+
+namespace ChallengeCompraGamer_Backend.Models.Micro.AssignChicos
+{
+    public class AssignChicosToMicroResponseDTO : GetMicroByPatenteResponseDTO
+    {
+    }
+}
